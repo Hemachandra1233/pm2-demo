ab0212acbb9e0c89f1d48eb47d277d4b86113bae
Hemachandra1233 <hemachandrachindam@gmail.com>
2023-12-15 13:21:00 +0530

    added

---
+++ source/ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+  }, {
+    script: 'index.js'
+  }],
+   
+  // Deployment Configuration
+  deploy : {
+    production : {
+       "user" : "ubuntu",
+       "host" : ["127.0.0.1"],
+       "ref"  : "origin/master",
+       "repo" : "git@github.com:Hemachandra1233/pm2-demo.git",
+       "path" : "/home/ubuntu/express-demo",
+       "post-deploy" : "npm pack"
+    }
+  }
+};
